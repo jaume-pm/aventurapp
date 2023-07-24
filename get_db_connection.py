ecb92ab1e06de09bd287c3052f8c78014e50a374
jaume_pm <jaumepetit2002@gmail.com>
2023-07-24 21:40:38 +0200

    Reorganise and create Docker for webserver

--- get_db_connection.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-def get_db_connection():
-    conn = psycopg2.connect(host='YOUR_HOST',
-                            database='YOUR_DATABASE',
-                            user='YOUR_USER',
-                            password='YOUR_PASSWORD')
-    return conn
