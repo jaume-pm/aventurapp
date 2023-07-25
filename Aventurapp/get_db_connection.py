c605d6a0a7a157486d4446fefea59a750fde5313
jaume_pm <jaumepetit2002@gmail.com>
2023-07-25 03:10:53 +0200

    Configure connection to psql Docker

--- Aventurapp/get_db_connection.py
+++ Aventurapp/get_db_connection.py
@@ -1,7 +1,11 @@
 import psycopg2
+import os
+
+Host = os.getenv('POSTGRES_HOST')
+
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='Aventurapp',
-                            user='user',
+    conn = psycopg2.connect(host = Host,
+                            port = '5432',
+                            user='postgres',
                             password='Aventurapp')
     return conn
